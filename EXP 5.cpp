dda81e27ab17da0cfdf5155beb7f6c0d73add734
komaldarisipudi <122425577+komaldarisipudi@users.noreply.github.com>
2023-01-25 09:50:21 +0530

    EXP 5
    Construct a scheduling program with C that selects the waiting process with the highest priority to execute next

---
+++ EXP 5.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_PROCESSES 10
+
+struct Process {
+    int pid;
+    int priority;
+    int burst_time;
+};
+
+int main() {
+    // Initialize an array of processes with sample data
+    struct Process processes[MAX_PROCESSES] = {
+        {1, 3, 5},
+        {2, 1, 4},
+        {3, 4, 2},
+        {4, 2, 6},
+        {5, 5, 1}
+    };
+
+    // Find the process with the highest priority
+    int highest_priority = 0;
+    int highest_priority_index = 0;
+    for (int i = 0; i < MAX_PROCESSES; i++) {
+        if (processes[i].priority > highest_priority) {
+            highest_priority = processes[i].priority;
+            highest_priority_index = i;
+        }
+    }
+
+    // Execute the process with the highest priority
+    printf("Executing process %d with priority %d and burst time %d\n", 
+        processes[highest_priority_index].pid, 
+        processes[highest_priority_index].priority, 
+        processes[highest_priority_index].burst_time);
+
+    return 0;
+}
